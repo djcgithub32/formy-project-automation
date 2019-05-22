4d1ba9b3c7cf87a3036faf096f0b6c237720043a
unknown <djcook32@optonline.net>
2019-05-22 15:16:17 -0400

    Initial commit

---
+++ 06_01/start/test/Form.java
@@ -0,0 +1,36 @@
+import org.apache.bcel.generic.RETURN;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Form {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/Owner/Downloads/chromedriver_win32/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        driver.findElement(By.id("first-name")).sendKeys("David");
+
+        driver.findElement(By.id("last-name")).sendKeys("Cook");
+
+        driver.findElement(By.id("job-title")).sendKeys("Software Tester");
+
+        driver.findElement(By.id("radio-button-2")).click();
+
+        driver.findElement(By.id("checkbox-1")).click();
+
+        driver.findElement(By.cssSelector("option[value='4']")).click();
+
+        driver.findElement(By.id("datepicker")).sendKeys("05/22/2019");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+
+        driver.quit();
+    }
+}
